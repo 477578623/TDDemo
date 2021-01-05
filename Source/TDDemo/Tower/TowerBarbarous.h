6d24e670d5c9f8b20ac8614124958ca77aeb4595
zhuyangyang <477578623@qq.com>
2021-01-05 23:52:32 +0800

    防御塔基类，野蛮人防御塔，野蛮人防御塔蓝图

---
+++ Source/TDDemo/Tower/TowerBarbarous.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseTower.h"
+#include "TowerBarbarous.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TDDEMO_API ATowerBarbarous : public ABaseTower
+{
+	GENERATED_BODY()
+	
+};
